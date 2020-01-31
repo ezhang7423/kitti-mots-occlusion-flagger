2110fab034d96acbea1f9f9a9ab0aa5e8677ebd9
Eddie Zhang <54998055+ezhang7423@users.noreply.github.com>
2020-01-31 12:38:41 -0800

    finish is occluded based on bounding box

--- flag.py
+++ flag.py
@@ -20,17 +20,41 @@ from iot import load_sequences, load_seqmap, load_txt
 
 
 def isOccluded(boxes):
+    x0 = []
+    xf = []
+    y0 = []
+    yf = []
     for x in range(len(boxes)):
         box = [i for i in boxes[x]]
-        print(len(box))
+        # x0 += [box[0]] move this to bottom of function
+        # xf += [box[2]]
+        x_index = [box[0], box[2]]
+        for i in range(len(x0)):
+            if x0[i] <= x_index[0] <= xf[i]:
+                return True
+            elif x[i] <= x_index[1] <= xf[i]:
+                return True
+        x0.append(x_index[0])
+        xf.append(x_index[2])
+
+        y_index = [box[1], box[3]]
+        for i in range(len(y0)):
+            if y0[i] <= y_index[0] <= yf[i]:
+                return True
+            elif y[i] <= y_index[1] <= yf[i]:
+                return True
+        y0.append(y_index[0])
+        yf.append(y_index[2])
+        # check if collision
         
-    return;
+    return False;
+
 
 def returnBoxes(frame):
     frame128 = frame
     bBoxes = []
     for x in range(len(frame128)):
-        if (frame128[x].class_id == 1 or frame128[x].class_id == 1):
+        if (frame128[x].class_id == 1 or frame128[x].class_id == 2):
             bBoxes.append(rletools.toBbox(frame128[x].mask))
     return bBoxes
 
@@ -38,7 +62,7 @@ if __name__ == "__main__":
     frames = load_txt("instance_sample.txt")
     occlusions = []
     # for x in range(len(frames)):
-    occlusions.append(isOccluded(returnBoxes(frames[x])))
+    occlusions.append(isOccluded(returnBoxes(frames[128])))
 
     
 
